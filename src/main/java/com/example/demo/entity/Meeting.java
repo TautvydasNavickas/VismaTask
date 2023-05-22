5ec603cace627038b3ceef3967fcbfe9d7a9034e
tautv <taut.navickas@gmail.com>
2023-05-22 21:56:48 +0300

    Repository was added

--- src/main/java/com/example/demo/entity/Meeting.java
+++ src/main/java/com/example/demo/entity/Meeting.java
@@ -1,9 +1,15 @@
 package com.example.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Entity
 public class Meeting {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String Name;
     private String ResponsiblePerson;
     private String Description;
