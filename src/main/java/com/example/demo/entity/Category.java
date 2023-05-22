9abdcf193b3d419a4b8b88544dc0343c33d33f5f
tautv <taut.navickas@gmail.com>
2023-05-22 21:39:31 +0300

    -

---
+++ src/main/java/com/example/demo/entity/Category.java
@@ -0,0 +1,8 @@
+package com.example.demo.entity;
+
+public enum Category {
+    CodeMonkey,
+    Hub,
+    Short,
+    TeamBuilding
+}
---
+++ src/main/java/com/example/demo/entity/Meeting.java
@@ -0,0 +1,81 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+
+public class Meeting {
+
+    private String Name;
+    private String ResponsiblePerson;
+    private String Description;
+    private Category category;
+    private Type type;
+    private LocalDate StartDate;
+    private LocalDate EndDate;
+
+
+    public Meeting(String name, String responsiblePerson, String description, Category category, Type type, LocalDate startDate, LocalDate endDate) {
+        Name = name;
+        ResponsiblePerson = responsiblePerson;
+        Description = description;
+        this.category = category;
+        this.type = type;
+        StartDate = startDate;
+        EndDate = endDate;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getResponsiblePerson() {
+        return ResponsiblePerson;
+    }
+
+    public void setResponsiblePerson(String responsiblePerson) {
+        ResponsiblePerson = responsiblePerson;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public LocalDate getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        StartDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return EndDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        EndDate = endDate;
+    }
+}
